10c64a60be526e20fc551eb0ff9ce61903eee909
Stefan Kramel <skramel@wesleyan.edu>
2017-02-27 14:03:55 -0500

    Initial Commit

---
+++ hello world/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello world
+//
+//  Created by Stefan Kramel on 2/27/17.
+//  Copyright © 2017 Stefan Kramel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
